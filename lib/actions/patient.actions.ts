cdd103a557cb29d9105fb29d0ab32afe18e3006e
George adedeji <georgeadedeji79@gmail.com>
2024-11-09 08:14:52 +0100

    first commit

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,44 @@
+
+
+
+let Query: typeof import('node-appwrite').Query;
+let users: typeof import('../appwrite.config').users;
+let ID: typeof import ('node-appwrite').ID
+import { parseStringify } from '../utils';
+// import { ID } from 'node-appwrite';
+
+if (typeof window === 'undefined') {
+  const appwrite = require('node-appwrite');
+  const appwriteConfig = require('../appwrite.config');
+  
+  Query = appwrite.Query;
+  users = appwriteConfig.users;
+  ID = appwrite.ID
+}
+
+
+export const createUser = async (user: CreateUserParams) => {
+    try {
+      const newUser = await users.create(
+        ID.unique(),
+        user.email, 
+        user.phone, 
+        undefined, 
+        user.name
+      );
+  
+      return parseStringify(newUser);
+    } catch (error: any) {
+      console.log("Error object:", error); 
+  
+      if (error && error?.code === 409) { 
+        const existingUser = await users.list([
+          Query.equal('email', [user.email]),
+        ]);
+        return existingUser?.users[0];
+      }
+  
+      console.error("An error occurred while creating a new user:", error);
+    }
+  };
+  
